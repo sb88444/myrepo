c8f535a50a1bc2a68fb27877db9f22d8742eedf6
sb88444 <sandesh.csb@gmail.com>
2023-02-02 11:49:35 +0530

    Update startup.sh

--- e2e_tests/sample/startup.sh
+++ e2e_tests/sample/startup.sh
@@ -4,8 +4,10 @@
 set -o xtrace
 SOURCE_DIR=$PWD
 
+docker build -t e2e_test -f BE-Dockerfile .
+
 echo "Executing Pytest"
-docker run --rm --entrypoint pytest --name ${username} -v $SOURCE_DIR:/stms/qa \
-           --network host e2e_pytest:latest /stms/qa/
+docker run --rm --entrypoint pytest --name test -v $SOURCE_DIR:/stms/qa \
+           --network host e2e_test:latest /stms/qa/
 
 # DO NOT ADD CODE AFTER docker run COMMAND AS OUTPUT OF THIS COMMAND RETURNS EXIT CODE BASED ON WHICH JOB STATUS IS UPDATED.
