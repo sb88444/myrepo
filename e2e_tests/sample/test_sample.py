cf9f6124264ce387076ecfb07225ae5f646f4287
sb88444 <sandesh.csb@gmail.com>
2023-02-02 16:00:27 +0530

    Update test_sample.py

--- e2e_tests/sample/test_sample.py
+++ e2e_tests/sample/test_sample.py
@@ -1,8 +1,5 @@
-import lib.common.global_config as global_config
 import logging
-from e2e_lib.common.delayed_assert import expect_and_log
 
-pylogger = global_config.pylogger
 
 
 class TestSample():
@@ -11,8 +8,4 @@ class TestSample():
 
 
     def test_sample(cls, caplog):
-        caplog.set_level(logging.CRITICAL)
-        expect_and_log("ABC" == "X", "request.node.name",
-                       "ABC == XYZ",
-                       "ABC",
-                       "ABC")
+        assert A == B
