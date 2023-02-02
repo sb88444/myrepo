14c422f03d7c3b9d904524a70783c22933a8f2dd
sb88444 <sandesh.csb@gmail.com>
2023-02-02 13:12:47 +0530

    Update startup.sh

--- e2e_tests/startup.sh
+++ e2e_tests/startup.sh
@@ -6,9 +6,10 @@ SOURCE_DIR=$PWD
 cd $SOURCE_DIR
 pwd
 ls
+docker login -u sandeshb8 -p dckr_pat__3CqYNrraAqPWzTRRDxUUB9mZVQ docker.io
 docker build -t e2e_test -f $SOURCE_DIR/e2e_tests/Dockerfile .
-
-docker run --rm --entrypoint=/bin/bash --name test -v $SOURCE_DIR:/stms/qa --network host e2e_test:latest -c ls
+docker push e2e_test
+docker run --rm --entrypoint=/bin/bash --name test -v $SOURCE_DIR/e2e_tests:/stms/qa --network host e2e_test:latest -c ls
 
 #echo "Executing Pytest"
 #docker run --rm --entrypoint pytest --name test -v $SOURCE_DIR:/stms/qa \
