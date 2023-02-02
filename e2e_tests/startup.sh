5e5ef5100afe61a3d73d4e8c9360c4a8ba5d4bff
sb88444 <sandesh.csb@gmail.com>
2023-02-02 12:32:25 +0530

    Update startup.sh

--- e2e_tests/startup.sh
+++ e2e_tests/startup.sh
@@ -11,6 +11,6 @@ docker build -t e2e_test -f $SOURCE_DIR/e2e_tests/Dockerfile .
 
 echo "Executing Pytest"
 docker run --rm --entrypoint pytest --name test -v $SOURCE_DIR:/stms/qa \
-           --network host e2e_test:latest /stms/qa/sample
+           --network host e2e_test:latest /stms/qa/e2e_tests/sample
 
 # DO NOT ADD CODE AFTER docker run COMMAND AS OUTPUT OF THIS COMMAND RETURNS EXIT CODE BASED ON WHICH JOB STATUS IS UPDATED.
